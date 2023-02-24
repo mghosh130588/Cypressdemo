c48942c98e1992920c38299f2359ba4f0f7ee2ab
mghosh130588 <mousumi.nit@gmail.com>
2023-02-24 16:28:30 +0000

    secon d commit

---
+++ cypress/integration/Day2/HandlingEach.js
@@ -0,0 +1,13 @@
+describe("Handling each function",()=>{
+
+    beforeEach(()=>{
+        cy.visit("https://opensource-demo.orangehrmlive.com/");
+    });
+
+it("check number of elements in dashboard",()=>{
+    cy.log("Starting the test");
+})
+
+
+
+})
